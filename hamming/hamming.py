9fa4fe1184b6993b2cdf4720642282114757cb68
Lucien Najev <luciennajev@hotmail.com>
2021-03-13 12:39:09 +0000

    new ex

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -3,8 +3,9 @@ def distance(strand_a, strand_b):
         raise ValueError("Error: DNA strands must have equal length")
 
     count = 0
-    for i, nucleotide in enumerate(strand_a):
-        if nucleotide != strand_b[i]:
+
+    for a, b in zip(strand_a, strand_b):
+        if a != b:
             count += 1
 
     return count
