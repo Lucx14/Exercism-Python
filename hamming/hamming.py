27c8fd8c88f06315554eab0156650c3be8c85253
Lucien Najev <luciennajev@hotmail.com>
2021-03-18 19:57:09 +0000

    correction

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,11 +1,21 @@
+"""Hamming distance calculator"""
+
+
 def distance(strand_a, strand_b):
+    """
+    Calculates the hamming distance
+
+    :param strand_a: str
+    :param strand_b: str
+    :return: int
+    """
     if len(strand_a) != len(strand_b):
         raise ValueError("Error: DNA strands must have equal length")
 
     count = 0
 
-    for a, b in zip(strand_a, strand_b):
-        if a != b:
+    for nuc_a, nuc_b in zip(strand_a, strand_b):
+        if nuc_a != nuc_b:
             count += 1
 
     return count
