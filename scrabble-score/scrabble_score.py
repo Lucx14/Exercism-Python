48e0c3c013b20c9c44c16b222b71b059af2dace9
Lucien Najev <luciennajev@hotmail.com>
2021-03-24 19:10:04 +0000

    new ex

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,21 @@
 def score(word):
-    pass
+    VALUES = (
+        ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"),
+        ("D", "G"),
+        ("B", "C", "M", "P"),
+        ("F", "H", "V", "W", "Y"),
+        ("K"),
+        (),
+        (),
+        ("J", "X"),
+        (),
+        ("Q", "Z"),
+    )
+    score = 0
+    for letter in word.upper():
+        for i, values in enumerate(VALUES):
+            if letter in values:
+                score += i + 1
+                break
+
+    return score
